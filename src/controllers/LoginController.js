ee9a98c2fcc2675721c280055c1271f9ec421175
sangie <saulblue30@gmail.com>
2023-11-20 21:24:40 -0600

    redireccion logout

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -107,7 +107,7 @@ function logout(req, res) {
   if (req.session.loggedin == true) {
     req.session.destroy();
   }
-  res.redirect('/login_i');
+  res.render('login/index');
 }
 
 //Renderiza la pagina de Personal y redirige hacia ella
